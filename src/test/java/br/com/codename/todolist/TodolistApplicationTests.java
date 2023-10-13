4ad9c88cf03e6b42fe8a2257ca917f3d598f9a57
Augusto Werlang <werlang10@gmail.com>
2023-10-12 23:23:12 -0300

    first commit

---
+++ src/test/java/br/com/codename/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codename.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
